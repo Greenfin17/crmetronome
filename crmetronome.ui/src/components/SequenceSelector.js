92d289cf62c8ebe88d78e76248318f201af8daa3
John Maple <jmaple@bellsouth.net>
2022-12-28 23:14:07 -0600

    starting code to select composer

--- crmetronome.ui/src/components/SequenceSelector.js
+++ crmetronome.ui/src/components/SequenceSelector.js
@@ -1,12 +1,16 @@
 import React, {useEffect} from 'react';
 import Select from 'react-select';
 import { propTypes } from 'prop-types';
+import getAllComposers from '../helpers/data/composerData';
 
 const SequenceSelector = (sequence) => {
   const [composerSelectOptions, setComposerSelectOptions] = useState(null);
   useEffect = () => {
     let mounted = true;
     if (mounted) {
+      getAllComposers().then(composerArray) => {
+
+      }
       
     }
 
---
+++ crmetronome.ui/src/helpers/data/sequenceData.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import {crmConfig} from '../apiKeys';
+
+const apiURL = crmConfig.apiURL;
+
+const 
